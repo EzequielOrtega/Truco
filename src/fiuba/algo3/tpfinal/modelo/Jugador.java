b8a89eaf863186450fba2e067475f91f20d15ab5
EzequielOrtega <ezequiel_ortegamateo@hotmail.com>
2015-11-10 18:03:08 -0300

    Jugador ya no calcula los tantos, ahora utiliza a CalculadorDeTantos para saberlos

--- src/fiuba/algo3/tpfinal/modelo/Jugador.java
+++ src/fiuba/algo3/tpfinal/modelo/Jugador.java
@@ -7,12 +7,16 @@ public class Jugador {
     // Tal vez deberia llamarse JugadorDeTruco o crear una clase JugadorDeTruco que herede/implemente esta.
 
     private final String nombre;
-    private Vector<Carta> cartas = new Vector<Carta>();
-    private int puntaje;
+    private Vector<Carta> cartas;
+    @SuppressWarnings("unused")
+	private int puntaje;
+    private CalculadorDeTantos calculador;
 
     public Jugador (String nombreJugador) {
         this.nombre = nombreJugador;
         this.puntaje = 0;
+        this.calculador = new CalculadorDeTantos();
+        this.cartas = new Vector<Carta>();
     }
 
     public String getNombre() {
@@ -48,33 +52,14 @@ public class Jugador {
 
     // ENVIDO Y FLOR:
     public int getValorEnvido () {
-        // No se si el chequeo de estar en 1ra ronda va aca o en JuegoDeTruco
         if (cartas.size() != 3)
             throw new CantidadDeCartasInvalidaError("El envido debe calcularse solo si se tiene 3 cartas en mano.");
-        if (cartas.get(0).mismoPaloQue(cartas.get(1), cartas.get(2)))
-            throw new NoTieneEnvidoTieneFlorError("Tiene 3 cartas del mismo palo, no puede cantar envido.");
-        int valorEnvido = 0;
-        if (cartas.get(0).mismoPaloQue(cartas.get(1)))
-            valorEnvido = 20 + valorEnEnvidoYFlor(cartas.get(0)) + valorEnEnvidoYFlor(cartas.get(1));
-        if (cartas.get(1).mismoPaloQue(cartas.get(2)))
-            valorEnvido = 20 + valorEnEnvidoYFlor(cartas.get(1)) + valorEnEnvidoYFlor(cartas.get(2));
-        if (cartas.get(2).mismoPaloQue(cartas.get(1)))
-            valorEnvido = 20 + valorEnEnvidoYFlor(cartas.get(2)) + valorEnEnvidoYFlor(cartas.get(1));
-        return valorEnvido;
+        return calculador.obtenerTantosDeEnvido(cartas);
     }
 
     public int getValorFlor () {
-        // No se si el chequeo de estar en 1ra ronda va aca o en JuegoDeTruco
         if (cartas.size() != 3)
             throw new CantidadDeCartasInvalidaError("El envido debe calcularse solo si se tiene 3 cartas en mano.");
-        int valorFlor = 0;
-        if (cartas.get(0).mismoPaloQue(cartas.get(1), cartas.get(2)))
-            valorFlor = 20 + valorEnEnvidoYFlor(cartas.get(0)) + valorEnEnvidoYFlor(cartas.get(1)) + valorEnEnvidoYFlor(cartas.get(2));
-        return valorFlor;
-    }
-
-    private int valorEnEnvidoYFlor(Carta unaCarta) {
-        if (unaCarta.esFigura())    return 0;
-        else                        return unaCarta.getValor();
+        return calculador.obtenerTantosDeFlor(cartas);
     }
 }
