7b0c3561e759d1a678df8df595e84c84d18a4d95
EzequielOrtega <ezequiel_ortegamateo@hotmail.com>
2015-11-10 17:37:59 -0300

    CalculadorDeTantos ya puede calcular flor y envido

--- src/fiuba/algo3/tpfinal/modelo/CalculadorDeTantos.java
+++ src/fiuba/algo3/tpfinal/modelo/CalculadorDeTantos.java
@@ -6,7 +6,7 @@ public class CalculadorDeTantos {
 	
 	public boolean tieneFlor(Vector<Carta> cartas){
 		if(cartas.size()<3){
-			throw new NoTieneSuficientesCartasParaCantarError();
+			throw new SoloSePuedeCantarEnPrimeraError();
 		}
 		Boolean tieneFlor = false;
 		for(Integer x = 0; x<2; x++){
@@ -33,5 +33,114 @@ public class CalculadorDeTantos {
 		}else{
 			throw new JugadorNoTieneFlorError();
 		}
+	}
+	
+	public Integer obtenerTantosDeEnvido(Vector<Carta> cartas){
+		if (cartas.size()<3){
+			throw new SoloSePuedeCantarEnPrimeraError();
+		}
+		Integer tantosDeEnvido;
+		if (this.esEnvidoDeTresCartas(cartas)){
+			tantosDeEnvido = this.calcularEnvidoDeTresCartas(cartas);
+		}else if(this.esEnvidoDeDosCartas(cartas)){
+			tantosDeEnvido = this.calcularEnvidoDeDosCartas(cartas);
+		}else{
+			tantosDeEnvido = this.calcularEnvidoDeUnaCarta(cartas);
+		}
+		return tantosDeEnvido;
+		
+	}
+
+
+	private Integer calcularEnvidoDeTresCartas(Vector<Carta> cartas) {
+		Integer tantosDeEnvido = 20;
+		ValorDeCartaParaEnvidoYFlor carta1 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(0);
+		ValorDeCartaParaEnvidoYFlor carta2 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(1);
+		ValorDeCartaParaEnvidoYFlor carta3 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(2);
+		Integer valor1 = carta1.obtenerValorParaEnvidoYFlor();
+		Integer valor2 = carta2.obtenerValorParaEnvidoYFlor();
+		Integer valor3 = carta3.obtenerValorParaEnvidoYFlor();
+		tantosDeEnvido += this.sumarLosDosValoresMasAltos(valor1, valor2, valor3);	
+		return tantosDeEnvido;
+	}
+
+
+	private Integer sumarLosDosValoresMasAltos(Integer valor1, Integer valor2, Integer valor3) {
+		Integer suma = 0;
+		int[] valores = new int[3];
+		valores[0] = valor1;
+		valores[1] = valor2;
+		valores[2] = valor3;
+		this.ordenarAscendientemente(valores);
+		suma += valores[1];
+		suma += valores[2];
+		return suma;
+	}
+
+
+	private void ordenarAscendientemente(int[] valores) {
+		int aux;
+		 
+	    for (int i = 0; i < valores.length - 1; i++) {
+	        for (int x = i + 1; x < valores.length; x++) {
+	            if (valores[x] < valores[i]) {
+	                aux = valores[i];
+	                valores[i] = valores[x];
+	                valores[x] = aux;
+	            }
+	        }
+	    }
+	}
+
+
+	private Integer calcularEnvidoDeDosCartas(Vector<Carta> cartas) {
+		Integer tantosDeEnvido = 20;
+		ValorDeCartaParaEnvidoYFlor carta1 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(0);
+		ValorDeCartaParaEnvidoYFlor carta2 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(1);
+		ValorDeCartaParaEnvidoYFlor carta3 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(2);
+		if(cartas.elementAt(0).getPalo() == cartas.elementAt(1).getPalo()) {
+			tantosDeEnvido += carta1.obtenerValorParaEnvidoYFlor();
+			tantosDeEnvido += carta2.obtenerValorParaEnvidoYFlor();
+		}else if(cartas.elementAt(1).getPalo() == cartas.elementAt(2).getPalo()){
+			tantosDeEnvido += carta2.obtenerValorParaEnvidoYFlor();
+			tantosDeEnvido += carta3.obtenerValorParaEnvidoYFlor();
+		}else{
+			tantosDeEnvido += carta1.obtenerValorParaEnvidoYFlor();
+			tantosDeEnvido += carta3.obtenerValorParaEnvidoYFlor();
+		}
+		return tantosDeEnvido;
+	}
+
+
+	private Integer calcularEnvidoDeUnaCarta(Vector<Carta> cartas) {
+		ValorDeCartaParaEnvidoYFlor carta1 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(0);
+		ValorDeCartaParaEnvidoYFlor carta2 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(1);
+		ValorDeCartaParaEnvidoYFlor carta3 = (ValorDeCartaParaEnvidoYFlor) cartas.elementAt(2);
+		Integer valor1 = carta1.obtenerValorParaEnvidoYFlor();
+		Integer valor2 = carta2.obtenerValorParaEnvidoYFlor();
+		Integer valor3 = carta3.obtenerValorParaEnvidoYFlor();
+		if((valor1 > valor2) && (valor1 > valor3)){
+			return valor1;
+		} else if ((valor2 > valor1) && (valor2 > valor3)){
+			return valor2;
+		}else{
+			return valor3;
+		}
+	}
+
+
+	private boolean esEnvidoDeDosCartas(Vector<Carta> cartas) {
+		Boolean comparacion1 = cartas.elementAt(0).getPalo() == cartas.elementAt(1).getPalo();
+		Boolean comparacion2 = cartas.elementAt(1).getPalo() == cartas.elementAt(2).getPalo();
+		Boolean esEnvidoDeDos = comparacion1 || comparacion2;
+		return esEnvidoDeDos;
+	}
+
+
+	private boolean esEnvidoDeTresCartas(Vector<Carta> cartas) {
+		Boolean comparacion1 = cartas.elementAt(0).getPalo() == cartas.elementAt(1).getPalo();
+		Boolean comparacion2 = cartas.elementAt(1).getPalo() == cartas.elementAt(2).getPalo();
+		Boolean esEnvidoDeTres = comparacion1 && comparacion2;
+		return esEnvidoDeTres;
 	}
 }
--- test/fiuba/algo3/tpfinal/tests/CalculadorDeTantosTest.java
+++ test/fiuba/algo3/tpfinal/tests/CalculadorDeTantosTest.java
@@ -11,7 +11,7 @@ import fiuba.algo3.tpfinal.modelo.Carta;
 import fiuba.algo3.tpfinal.modelo.Figura;
 import fiuba.algo3.tpfinal.modelo.JugadorNoTieneFlorError;
 import fiuba.algo3.tpfinal.modelo.NoFigura;
-import fiuba.algo3.tpfinal.modelo.NoTieneSuficientesCartasParaCantarError;
+import fiuba.algo3.tpfinal.modelo.SoloSePuedeCantarEnPrimeraError;
 
 public class CalculadorDeTantosTest {
 	private CalculadorDeTantos calculador;
@@ -42,7 +42,7 @@ public class CalculadorDeTantosTest {
 		cartas.addElement(carta3);
 		Assert.assertFalse(calculador.tieneFlor(cartas));
 	}
-	@Test (expected = NoTieneSuficientesCartasParaCantarError.class)
+	@Test (expected = SoloSePuedeCantarEnPrimeraError.class)
 	public void testTieneFlorFallaSiFaltanCartas(){
 		Carta carta1 = new NoFigura(1, "espada");
 		Carta carta2 = new NoFigura(7, "copa");
@@ -72,5 +72,48 @@ public class CalculadorDeTantosTest {
 		cartas.addElement(carta2);
 		cartas.addElement(carta3);
 		calculador.obtenerTantosDeFlor(cartas);
+	}
+	@Test
+	public void testObtenerTantosDeEnvidoDeTresCartas(){
+		Carta carta1 = new NoFigura(1, "espada");
+		Carta carta2 = new NoFigura(7, "espada");
+		Carta carta3 = new NoFigura(5, "espada");
+		Vector<Carta> cartas = new Vector<Carta>();
+		cartas.addElement(carta1);
+		cartas.addElement(carta2);
+		cartas.addElement(carta3);
+		Assert.assertTrue(calculador.obtenerTantosDeEnvido(cartas) == 32);
+	}
+	@Test
+	public void testObtenerTantosDeEnvidoDeDosCartas(){
+		Carta carta1 = new NoFigura(1, "basto");
+		Carta carta2 = new NoFigura(7, "espada");
+		Carta carta3 = new NoFigura(5, "espada");
+		Vector<Carta> cartas = new Vector<Carta>();
+		cartas.addElement(carta1);
+		cartas.addElement(carta2);
+		cartas.addElement(carta3);
+		Assert.assertTrue(calculador.obtenerTantosDeEnvido(cartas) == 32);
+	}
+	@Test
+	public void testObtenerTantosDeEnvidoDeUnaCarta(){
+		Carta carta1 = new NoFigura(1, "copa");
+		Carta carta2 = new NoFigura(7, "basto");
+		Carta carta3 = new NoFigura(5, "espada");
+		Vector<Carta> cartas = new Vector<Carta>();
+		cartas.addElement(carta1);
+		cartas.addElement(carta2);
+		cartas.addElement(carta3);
+		Assert.assertTrue(calculador.obtenerTantosDeEnvido(cartas) == 7);
+	}
+	@Test (expected = SoloSePuedeCantarEnPrimeraError.class)
+	public void testObtenerTantosDeEnvidoConMenosDeTresCartas(){
+		Carta carta1 = new NoFigura(1, "espada");
+		Carta carta2 = new NoFigura(7, "espada");
+		Vector<Carta> cartas = new Vector<Carta>();
+		cartas.addElement(carta1);
+		cartas.addElement(carta2);
+		calculador.obtenerTantosDeEnvido(cartas);
+		
 	}
 }
