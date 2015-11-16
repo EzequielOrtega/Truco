47534fd0a749c503482857211f13f9a72944cb3c
Marcos <mpozzo94@hotmail.com>
2015-11-16 19:47:19 -0300

    refactoring de CalculadorDeTantosTest: nombres mas descriptivos.

--- test/fiuba/algo3/tpfinal/tests/CalculadorDeTantosTest.java
+++ test/fiuba/algo3/tpfinal/tests/CalculadorDeTantosTest.java
@@ -10,79 +10,79 @@ import static org.junit.Assert.*;
 
 public class CalculadorDeTantosTest {
 	private CalculadorDeTantos calculador;
-	private Carta carta1, carta2, carta3, carta4, carta5, carta6, carta7;
+	private Carta unoDeEspada, sieteDeEspada, diezDeEspada, sieteDeCopa, diezDeBasto, cincoDeEspada, unoDeBasto;
     private Vector<Carta> cartas = new Vector<Carta>();
 	
 	@Before
 	public void setup(){
         calculador = new CalculadorDeTantos();
-        carta1 = new NoFigura(1, Palo.ESPADA);
-        carta2 = new NoFigura(7, Palo.ESPADA);
-        carta3 = new Figura(10, Palo.ESPADA);
-        carta4 = new NoFigura(7, Palo.COPA);
-        carta5 = new Figura(10, Palo.BASTO);
-        carta6 = new NoFigura(5, Palo.ESPADA);
-        carta7 = new NoFigura(1, Palo.BASTO);
+        unoDeEspada = new NoFigura(1, Palo.ESPADA);
+        sieteDeEspada = new NoFigura(7, Palo.ESPADA);
+        diezDeEspada = new Figura(10, Palo.ESPADA);
+        sieteDeCopa = new NoFigura(7, Palo.COPA);
+        diezDeBasto = new Figura(10, Palo.BASTO);
+        cincoDeEspada = new NoFigura(5, Palo.ESPADA);
+        unoDeBasto = new NoFigura(1, Palo.BASTO);
 	}
 	@Test
 	public void testTieneFlor() {
-		cartas.add(carta1);
-		cartas.add(carta2);
-		cartas.add(carta3);
+		cartas.add(unoDeEspada);
+		cartas.add(sieteDeEspada);
+		cartas.add(diezDeEspada);
 		assertTrue(calculador.tieneFlor(cartas));
 	}
 	@Test
 	public void testNoTieneFlor() {
-		cartas.add(carta1);
-		cartas.add(carta4);
-		cartas.add(carta5);
+		cartas.add(unoDeEspada);
+		cartas.add(sieteDeCopa);
+		cartas.add(diezDeBasto);
 		assertFalse(calculador.tieneFlor(cartas));
 	}
 	@Test (expected = SoloSePuedeCantarEnPrimeraError.class)
 	public void testTieneFlorFallaSiFaltanCartas(){
-		cartas.add(carta1);
-		cartas.add(carta4);
+		cartas.add(unoDeEspada);
+		cartas.add(sieteDeCopa);
 		calculador.tieneFlor(cartas);
 	}
 	@Test
 	public void testObtenerTantosDeFlor(){
-		cartas.add(carta1);
-		cartas.add(carta2);
-		cartas.add(carta3);
+		cartas.add(unoDeEspada);
+		cartas.add(sieteDeEspada);
+		cartas.add(diezDeEspada);
 		assertEquals(28, calculador.obtenerTantosDeFlor(cartas));
 	}
 	@Test (expected = JugadorNoTieneFlorError.class)
 	public void testObtenerTantosDeFlorSinFLorError(){
-		cartas.add(carta1);
-		cartas.add(carta4);
-		cartas.add(carta5);
+		cartas.add(unoDeEspada);
+		cartas.add(sieteDeCopa);
+		cartas.add(diezDeBasto);
 		calculador.obtenerTantosDeFlor(cartas);
 	}
 	@Test
 	public void testObtenerTantosDeEnvidoDeTresCartas(){
-		cartas.addElement(carta1);
-		cartas.addElement(carta2);
-		cartas.addElement(carta6);
+		cartas.addElement(unoDeEspada);
+		cartas.addElement(sieteDeEspada);
+		cartas.addElement(cincoDeEspada);
 		assertEquals(33, calculador.obtenerTantosDeEnvido(cartas));
 	}
 	@Test
 	public void testObtenerTantosDeEnvidoDeDosCartas(){
-		cartas.add(carta7);
-		cartas.add(carta2);
-		cartas.add(carta6);
+		cartas.add(unoDeBasto);
+		cartas.add(sieteDeEspada);
+		cartas.add(cincoDeEspada);
 		assertEquals(32, calculador.obtenerTantosDeEnvido(cartas));
 	}
 	@Test
 	public void testObtenerTantosDeEnvidoDeUnaCarta(){
-		cartas.add(carta4);
-		cartas.add(carta6);
-		cartas.add(carta7);
+		cartas.add(sieteDeCopa);
+		cartas.add(cincoDeEspada);
+		cartas.add(unoDeBasto);
 		assertEquals(7, calculador.obtenerTantosDeEnvido(cartas));
 	}
 	@Test (expected = SoloSePuedeCantarEnPrimeraError.class)
 	public void testObtenerTantosDeEnvidoConMenosDeTresCartas(){
-		cartas.add(carta1);
-		cartas.add(carta2);
+		cartas.add(unoDeEspada);
+		cartas.add(sieteDeEspada);
 		calculador.obtenerTantosDeEnvido(cartas);
 		
 	}
