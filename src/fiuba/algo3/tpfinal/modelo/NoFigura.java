14f7a6705c76edef23ba3116b922f667b84e8e13
EzequielOrtega <ezequiel_ortegamateo@hotmail.com>
2015-11-20 01:36:25 -0300

    Refactoring NoFigura

--- src/fiuba/algo3/tpfinal/modelo/NoFigura.java
+++ src/fiuba/algo3/tpfinal/modelo/NoFigura.java
@@ -4,6 +4,9 @@ public class NoFigura extends Carta implements CartaParaEnvidoYFlor {
 	
 	public NoFigura (Integer valor, Palo palo){
 		super(valor, palo);
+		if(valor > 7){
+			throw new ValorDeCartaInvalidoError();
+		}
 	}
 	@Override
 	public int obtenerValorParaEnvidoYFlor() {
---
+++ test/fiuba/algo3/tpfinal/tests/NoFiguraTest.java
@@ -0,0 +1,24 @@
+package fiuba.algo3.tpfinal.tests;
+
+import org.junit.Test;
+
+import fiuba.algo3.tpfinal.modelo.Carta;
+import fiuba.algo3.tpfinal.modelo.NoFigura;
+import fiuba.algo3.tpfinal.modelo.Palo;
+import fiuba.algo3.tpfinal.modelo.ValorDeCartaInvalidoError;
+
+public class NoFiguraTest {
+
+	@Test	(expected = ValorDeCartaInvalidoError.class)
+	public void testValorMayorASiete() {
+		@SuppressWarnings("unused")
+		Carta carta = new NoFigura(8, Palo.ESPADA);
+	}
+	
+	@Test (expected = ValorDeCartaInvalidoError.class)
+	public void testValorMenorAUno(){
+		@SuppressWarnings("unused")
+		Carta carta = new NoFigura(0, Palo.ESPADA);
+	}
+
+}
