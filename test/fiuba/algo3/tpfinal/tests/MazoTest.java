6613374351eb78f4c5aca17ad8da14fd5efa2965
EzequielOrtega <ezequiel_ortegamateo@hotmail.com>
2015-11-19 22:25:37 -0300

    Refactoring Carta

--- test/fiuba/algo3/tpfinal/tests/MazoTest.java
+++ test/fiuba/algo3/tpfinal/tests/MazoTest.java
@@ -1,26 +1,45 @@
 package fiuba.algo3.tpfinal.tests;
 
 import fiuba.algo3.tpfinal.modelo.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.Vector;
+
 
 public class MazoTest {
 
-    private Mazo nuevoMazo = new Mazo();
+    private Mazo nuevoMazo;
+    
+    @Before
+    public void setup(){
+    	this.nuevoMazo = new Mazo();
+    }
 
     @Test
     public void testCrearMazoExitoso() {
         assertEquals(40, nuevoMazo.cantidadDeCartasRestantes());
     }
-
+    
+    @Ignore
     @Test
     public void testMezclarMazoExitoso() {
-       /* Vector<Carta> cartasOrdenadas = nuevoMazo.getCartas();
+    	nuevoMazo.mezclar();
+    	Vector<Carta> orden1 = nuevoMazo.getCartas();
         nuevoMazo.mezclar();
-        Vector<Carta> cartasDesordenadas = nuevoMazo.getCartas();
-        //assertFalse(cartasOrdenadas.equals(cartasDesordenadas));*/
+        Vector<Carta> orden2 = nuevoMazo.getCartas();
+        Boolean cartasMezcladas = false;
+        for(Integer x = 0; x < 40; x++){
+        	if(!orden1.get(x).equals(orden2.get(x))){
+        		cartasMezcladas = true;
+        	}
+        }
+        Assert.assertTrue(cartasMezcladas);
     }
 
     @Test
@@ -30,8 +49,8 @@ public class MazoTest {
         Carta carta2 = nuevoMazo.agarrarCarta();
         assertEquals(12, carta1.getValor());
         assertEquals(11, carta2.getValor());
-        assertEquals("ESPADA", carta1.getPalo());
-        assertEquals("ESPADA", carta2.getPalo());
+        assertEquals(Palo.ESPADA, carta1.getPalo());
+        assertEquals(Palo.ESPADA, carta2.getPalo());
     }
 
     @Test (expected = NoHayMasCartasError.class)
