b9a209e6e2a91b543c9108efe391d2b3a7ed0bb8
mcoul <mleancole@icloud.com>
2015-11-29 23:49:09 -0300

    probando cambios vista

--- src/fiuba/algo3/tpfinal/vista/Programa.java
+++ src/fiuba/algo3/tpfinal/vista/Programa.java
@@ -15,7 +15,6 @@ public class Programa extends Application {
     }
 
     private Stage principal;
-    private BorderPane disenioRaiz;
     private JuegoDeTruco juego;
     private Equipo equipo1;
     private Equipo equipo2;
@@ -31,22 +30,29 @@ public class Programa extends Application {
         grid.setVgap(8);
         grid.setHgap(10);
 
+        ChoiceBox<String> cantidadJugadores = new ChoiceBox<>();
+        cantidadJugadores.getItems().addAll("2 jugadores", "4 jugadores");
+        cantidadJugadores.setValue("2 jugadores");
+        GridPane.setConstraints(cantidadJugadores, 1, 0);
+        //cantidadJugadores.getSelectionModel().selectedItemProperty().addListener( (v, valAnterior, valNuevo) -> {
+        //});
+
         Label j1Label = new Label("Jugador 1: ");
-        GridPane.setConstraints(j1Label, 0, 0);
+        GridPane.setConstraints(j1Label, 0, 1);
 
         TextField j1Input = new TextField();
         j1Input.setPromptText("Ingrese un nombre");
-        GridPane.setConstraints(j1Input, 1, 0);
+        GridPane.setConstraints(j1Input, 1, 1);
 
         Label j2Label = new Label("Jugador 2::");
-        GridPane.setConstraints(j2Label, 0, 1);
+        GridPane.setConstraints(j2Label, 0, 2);
 
         TextField j2Input = new TextField();
         j2Input.setPromptText("Ingrese un nombre");
-        GridPane.setConstraints(j2Input, 1, 1);
+        GridPane.setConstraints(j2Input, 1, 2);
 
         CheckBox conFlor = new CheckBox("Con Flor");
-        GridPane.setConstraints(conFlor, 1, 2);
+        GridPane.setConstraints(conFlor, 1, 3);
         Button nuevoJuegoButton = new Button("Iniciar juego");
         nuevoJuegoButton.setOnAction(e -> {
             System.out.println("Nombre j1: " + j1Input.getText());
@@ -54,10 +60,10 @@ public class Programa extends Application {
             JuegoDeTruco juego = new JuegoDeTruco(j1Input.getText(), j2Input.getText());
             //handleOptions(conFlor);
         });
-        GridPane.setConstraints(nuevoJuegoButton, 1, 3);
+        GridPane.setConstraints(nuevoJuegoButton, 1, 4);
 
         // Agrego al gridPane
-        grid.getChildren().addAll(j1Label, j1Input, j2Label, j2Input, conFlor, nuevoJuegoButton);
+        grid.getChildren().addAll(cantidadJugadores, j1Label, j1Input, j2Label, j2Input, conFlor, nuevoJuegoButton);
 
         Scene scene = new Scene(grid, 300, 200);
         principal.setScene(scene);
