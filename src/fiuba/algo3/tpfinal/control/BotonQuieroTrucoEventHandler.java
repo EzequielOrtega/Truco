8b4ce959cf3c6fb06ba6c76fbe252d07751b4dab
EzequielOrtega <ezequiel_ortegamateo@hotmail.com>
2015-11-30 14:35:31 -0300

    Subo BotonQuieroTruco

---
+++ src/fiuba/algo3/tpfinal/control/BotonQuieroTrucoEventHandler.java
@@ -0,0 +1,23 @@
+package fiuba.algo3.tpfinal.control;
+
+import fiuba.algo3.tpfinal.modelo.JuegoDeTruco;
+import fiuba.algo3.tpfinal.vista.Programa;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class BotonQuieroTrucoEventHandler implements EventHandler<ActionEvent> {
+	
+	private JuegoDeTruco juego;
+	private Programa programa;
+	
+	public BotonQuieroTrucoEventHandler(JuegoDeTruco juego, Programa programa) {
+		this.programa = programa;
+		this.juego = juego;
+	}
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		juego.quieroTruco();
+		//programa.seCanto("");
+	}
+}
